2ecd9fd89717f066f7644375cc734ee4f181e49b
asamerry <asamerry72@gmail.com>
2026-02-14 21:38:05 -0800

    added view confidence for black-litterman returns

--- utils.py
+++ utils.py
@@ -28,10 +28,15 @@ def get_prices_data(asset_file, data_col, period, interval, recache):
 def is_asset(tok, prices):
     return tok in prices.columns
 
-def is_valid_view(toks, prices):
+def is_valid_view(view, prices):
+    toks = view.split("; ")
+    if len(toks) != 2: return False
+    else: toks, conf = toks
+    toks = toks.split()
+    
     if not is_asset(toks[0], prices): return False 
     
-    try: float(toks[1])
+    try: float(toks[1]); float(conf)
     except ValueError: return False
     
     if toks[2] not in ["up", "down", "over", "under"]:
@@ -44,14 +49,16 @@ def is_valid_view(toks, prices):
 
     return True
 
-def parse_view(toks, returns):
+def parse_view(view, returns):
+    toks, conf = view.split("; ")
+    toks = toks.split(); conf = float(conf)
     if len(toks) == 3:
-        dir = 1 if toks[2] == "up" else -1
+        dir = conf if toks[2] == "up" else -conf
         picks = [dir if toks[0] == abbr else 0 for abbr in returns.columns]
         return float(toks[1]), picks
     else:
         abbrs = [toks[0], toks[3]]
-        dirs = [1, -1] if toks[2] == "over" else [-1, 1]
+        dirs = [conf, -conf] if toks[2] == "over" else [-conf, conf]
         picks = []
         for abbr in returns.columns:
             if abbr == abbrs[0]: picks.append(dirs[0])
@@ -66,14 +73,14 @@ def get_views(views_file, returns):
         views = file.read().strip().split("\n")
 
     for view in views:
-        toks = view.split(" ")
-        if not is_valid_view(toks, returns): 
-            print(f"Invalid view statement: {view}.")
+        if not is_valid_view(view, returns): 
+            print(f"Invalid view statement: \"{view}\". Exiting.")
+            exit(1)
         else:
-            weight, picks = parse_view(toks, returns)
+            weight, picks = parse_view(view, returns)
             Q.append(weight)
             P.append(picks)
-    
+
     return np.array(Q).reshape(-1, 1), np.array(P)
 
 def get_market_weights(returns, recache):
